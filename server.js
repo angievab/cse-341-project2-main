d062b01a8a96ca9e91d374bf365e6144da6ab4b7
angievab <angiepincock@gmail.com>
2024-10-14 02:16:01 -0500

    dotenv

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
